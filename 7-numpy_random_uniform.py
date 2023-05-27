225b599d9eb1b03ddff76981ab59f0977e6bcad5
Taha <65498764+tahasimsek-tr@users.noreply.github.com>
2023-05-27 15:31:58 +0300

    Add files via upload

---
+++ 7-numpy_random_uniform.py
@@ -0,0 +1,7 @@
+#Test için büyük veri kümeleri oluşturmak --> 'random.uniform'
+
+import numpy
+
+x = numpy.random.uniform(0.0, 1.0, 100) #0.0dan 1.0 arasında rastgele 100 tane sayı seçmek.
+
+print(x)
